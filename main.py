201a35fcf65546268230c3689edcf3ec66b7f3e6
Volodymyr Vovnenko <vovnenko.volodymyr@gmail.com>
2024-12-01 22:01:14 +0200

    add CORS

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.api import utils, contacts, auth, users
 
 app = FastAPI()
+
+origins = ["http://localhost:8000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 API_PREFIX = "/api"
 app.include_router(utils.router, prefix=API_PREFIX)
